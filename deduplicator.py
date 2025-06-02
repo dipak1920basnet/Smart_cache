0cbc6eb5244bb6088b03cd40d329a216d4cc185f
dipak1920basnet <dipak1920basnet@gmail.com>
2025-06-02 22:28:38 +0545

    function to clean text is written

--- deduplicator.py
+++ deduplicator.py
@@ -1,24 +1,32 @@
 # import hashlib
 from simhash import Simhash
+from cleantext import clean
+
 
 class Deduplicator:
-
     def __init__(self):
         self.fingerprints = set()
 
+    def clean_text(text):
+        # write a function to clean text
+        return clean(
+            text,
+            clean_all=False,
+            lowercase=True,
+            extra_spaces=True,
+            stopwords=True,
+            punct=True,
+            stemming=True,
+        )
+
     def _hash(self, text):
+        # hashlib.md5(text.encode()).hexdigest()
+        # return Simhash(text)
+        cleaned_text = self.clean_text(text)
 
-        def clean_text(text):
-            # write a function to clean text
-            # hashlib.md5(text.encode()).hexdigest()
-            # return Simhash(text)
-            ...
+        # write a function to produce hash
+        return Simhash(cleaned_text)
 
-        cleaned_text = clean_text(text)
-
-        # write a function to produce hash 
-        return Simhash(text)
-    
     def is_duplicate(self, text):
         # returns true if hash exists in set
         # this threshold 6 was selected assuming it would work for all short to long text
@@ -28,12 +36,7 @@ class Deduplicator:
             if i.distance(hash_value) <= threshold:
                 return True
         return False
-        
 
     def add(self, text):
         produced_hash = self._hash(text)
         self.fingerprints.add(produced_hash)
-
-    
-
-    
