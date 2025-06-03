a176890e8cb073afa079d644ef7cfbcfbb743812
dipak1920basnet <dipak1920basnet@gmail.com>
2025-06-03 19:41:35 +0545

    get the frequency count and rank document by frequency

--- search_ranker.py
+++ search_ranker.py
@@ -3,11 +3,29 @@ import heapq
 class SearchRanker:
 
     def __init__(self):
-        ...
+        pass
 
     def score(self, text, keyword):
-        ...
+        # count frequency of keyword in given text
+        text = text.lower().strip()
+        keyword = keyword.lower().strip()
+        return text.split().count(keyword)
 
-    def rank(self, ):
-        ...
-    
+    def rank(self, documents:dict, keyword, k):
+        # use a heap to return top-k doc ID by score
+        heap = []
+        for doc_id, text in documents.items():
+            # get score
+            score = self.score(text, keyword)
+            # push (-score, doc_id) to heap to get max-heap behavior
+            heap.append((-score,doc_id))
+        heapq.heapify(heap)
+
+        top_documents = []
+        # Pop top-k and return sorted list of doc_ids
+        for i in range(min(k, len(heap))):
+            scores, document_id = heapq.heappop(heap)
+            top_documents.append(document_id)
+
+        return top_documents
+        
