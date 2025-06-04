da7097c13abdfc1b50bb9c847a04cbce1c3a0d1c
dipak1920basnet <dipak1920basnet@gmail.com>
2025-06-05 03:59:08 +0545

    build coding template

---
+++ suggestion_engine.py
@@ -0,0 +1,24 @@
+import Levenshtein
+
+class SuggestionEngine:
+
+    def __init__(self):
+        pass
+
+    def suggest(self, queries, current_query):
+        # Sliding window / two pointers to find substring overlaps
+        # Common prefix, suffix, or words in common
+        pass
+
+    # optional helper function
+    def longest_common_substring():
+        ...
+
+    # optional helper function two
+    def shared_words(current_query, past_query):
+        ...
+    
+
+    
+
+    
